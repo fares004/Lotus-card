f9356f831bdd07316bc9862feb269f74bda58a64
Fares Moaz Fares <fares.sy.30050@gmail.com>
2024-12-04 15:27:54 +0300

    Upload the site

---
+++ script.js
@@ -0,0 +1,8 @@
+const typed = new  Typed ('.multiple-text', {
+    strings:['نكهة تجمع بين الفن والطعم المميز'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop : true
+
+ });
